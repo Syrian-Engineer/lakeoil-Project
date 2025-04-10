6e9fd4ee0017c6d8700b8d0789c5d7dd49596f99
Syrian-Engineer <koussaikareem@gmail.com>
2025-04-10 17:59:19 +0300

    fixing bugs 15

--- src/ui/file-upload/invoice-image-uploader.tsx
+++ src/ui/file-upload/invoice-image-uploader.tsx
@@ -2,7 +2,7 @@
 
 import { Text } from "rizzui";
 import Image from "next/image";
-import cn from "../../utils/class-names";
+import cn from "@/utils/class-names";
 import UploadIcon from "../../components/shape/upload";
 import React, { ChangeEvent, useState } from "react";
 import { PiPencilBold, PiTrashBold } from "react-icons/pi";
