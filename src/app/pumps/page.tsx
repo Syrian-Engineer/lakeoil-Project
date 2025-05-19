0166df84d17030da7381525f3a93ac35ec2e19b8
Syrian-Engineer <koussaikareem@gmail.com>
2025-05-19 13:37:42 +0300

    fixing useSearchParms

--- src/app/pumps/page.tsx
+++ src/app/pumps/page.tsx
@@ -4,9 +4,9 @@ import { useSearchParams } from 'next/navigation';
 import PumpCardsGrid from './component/PumpCardsGrid';
 
 export default function page() {
-  const searchParams = useSearchParams();
-  const name = searchParams.get("name"); // From URL: ?name=Pump1
-
+  // const searchParams = useSearchParams();
+  // const name = searchParams.get("name"); // From URL: ?name=Pump1
+  const name = ""
   return (
     <div className="grid grid-cols-1 gap-6 @container 3xl:gap-8">
       <PumpCardsGrid selectedPumpName={name} />
