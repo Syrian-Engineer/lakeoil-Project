a43646bf25018c5ec0c5e6768b347c635e76dacd
Syrian-Engineer <koussaikareem@gmail.com>
2025-09-13 10:19:17 +0300

    adding functions for colors in tank page

--- src/types/d3-interpolate.d.ts
+++ src/types/d3-interpolate.d.ts
@@ -1,1 +1,9 @@
 declare module "d3-interpolate";
+declare module "d3-interpolate" {
+  export function interpolateRgb(a: string, b: string): (t: number) => string;
+  export function interpolateNumber(a: number, b: number): (t: number) => number;
+  export function interpolateString(a: string, b: string): (t: number) => string;
+
+  // Add other interpolators if needed
+  export function interpolate(a: any, b: any): (t: number) => any;
+}
