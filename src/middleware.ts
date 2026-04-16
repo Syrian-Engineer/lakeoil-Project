73ccf6eb6d4cf902bb184d551d19065f0b162353
Syrian-Engineer <koussaikareem@gmail.com>
2026-04-16 19:53:30 +0300

    fixing middle ware 4

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,57 +1,18 @@
-// import { NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
-
-// export function middleware(request: NextRequest) {
-//   const token = request.cookies.get("access_token")?.value;
-
-//   const isAuthPage = request.nextUrl.pathname.startsWith("/signin");
-
-//   // ❌ Not logged in → redirect to signin
-//   if (!token && !isAuthPage) {
-//     return NextResponse.redirect(new URL("/signin", request.url));
-//   }
-
-//   // ✅ Logged in → prevent going back to signin
-//   if (token && isAuthPage) {
-//     return NextResponse.redirect(new URL("/reports", request.url));
-//   }
-
-//   return NextResponse.next();
-// }
-
-
-
-// export const config = {
-//   matcher: [
-//     "/reports/:path*",
-//     "/reports/preodic-reports/:path*",
-//     "/",
-//     "/daily-reports/:path*",
-//     "/station/:path*",
-//     "/customers/:path*",
-//     "/settings/:path*",
-//     "/staff/:path*",
-//     "/tanks/:path*",
-//   ],
-// };
-
-
-
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const token = request.cookies.get("access_token")?.value;
 
-  // const isAuthPage = request.nextUrl.pathname.startsWith("/signin");
+  const isAuthPage = request.nextUrl.pathname.startsWith("/signin");
 
   // ❌ Not logged in → redirect to signin
-  if (!token) {
+  if (!token && !isAuthPage) {
     return NextResponse.redirect(new URL("/signin", request.url));
   }
 
   // ✅ Logged in → prevent going back to signin
-  if (token) {
+  if (token && isAuthPage) {
     return NextResponse.redirect(new URL("/reports", request.url));
   }
 
@@ -73,3 +34,5 @@ export const config = {
     "/tanks/:path*",
   ],
 };
+
+
