dfcf9d7984a7d93d5282d7f2e44aac7af3ab8594
Syrian-Engineer <koussaikareem@gmail.com>
2026-06-27 23:34:07 +0300

    fixing the staff page backend url

--- src/app/api/staff/route.ts
+++ src/app/api/staff/route.ts
@@ -2,8 +2,20 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   try {
-    const access_token = req.headers.get("Authorization")
-    const res = await fetch("http://central.oktin.ak4tek.com:3950/auth/protected", {
+    const access_token = req.headers.get("Authorization");
+    const backendUrl = req.headers.get("x-backend-url");
+
+      if (!access_token) {
+        return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+      }
+
+    if (!backendUrl) {
+        return NextResponse.json(
+          { error: "Backend not selected" },
+          { status: 400 }
+        );
+    }
+    const res = await fetch(`${backendUrl}/auth/protected`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
